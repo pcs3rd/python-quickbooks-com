1144f8677d076432fa6c19a2de45dcd5bc0b7756
pcs3rd <rhean1620@gmail.com>
2020-07-16 11:47:22 -0400

    added functions
    itemquery xml string changed.
    added call inventory
    I hope this works ootb.

--- python-quickbooks-com/com.py
+++ python-quickbooks-com/com.py
@@ -8,23 +8,28 @@ def open(): #Open a connection, instantate QBPOSXMLRPLib
     qb = win32.gencache.EnsureDispatch("QBPOSXMLRPLib.RequestProcessor")
     qb.OpenConnection("python-quickbooks-com", "REST api application for quickbooks.")
     return qb
+
 def begin(qb): #Begin a session
         ticket = qb.BeginSession("")
         return ticket
+    
 def close(qb, ticket): #Drop the session. Just like the now broken microphone.
     qb.EndSession(ticket)
     qb.CloseConnection()
-
+    #Do not return anything for the time being. It is not needed... At least I don't think... to include an exit code.
+    
 #API functions
-#Request item information. This should return some data?
-def itemquery(qb, ticket, data):
-    xmlstream = "<?qbposxml version=\"1.0\"?>\n<QBPOSXML>\n  <QBPOSXMLMsgsRq onError=\"stopOnError\">\n     <ItemInventoryQueryRq>" , str(data) , "</ItemInventoryQueryRq>\n  </QBPOSXMLMsgsRq>\n</QBPOSXML>"
+def itemquery(qb, ticket, data): #Request item information. This should return some data?
+    xmlstream = "    <?qbposxml version=\"1.0\"?>\n    <QBPOSXML>\n       <QBPOSXMLMsgsRq onError=\"stopOnError\">\n          <ItemInventoryQueryRq>\n             <ItemNumberFilter> \n             <MatchNumericCriterion >Equal</MatchNumericCriterion>\n             <ItemNumber >" + str(data) + "</ItemNumber>\n              </ItemNumberFilter>\n          </ItemInventoryQueryRq>\n       </QBPOSXMLMsgsRq>\n    </QBPOSXML>"
     #debug print(xmlstream)
     response = qb.ProcessRequest(ticket, xmlstream)
     return response
 
+def inventory(qb, ticket): #prints entire inventory. May be good for making a backup of a QB database. Don't know. What I do know is that it's piped output makes a finicky .txt file. Kinda weird.
+    xmlstream = "<?qbposxml version=\"1.0\"?>\n<QBPOSXML>\n  <QBPOSXMLMsgsRq onError=\"stopOnError\">\n     <ItemInventoryQueryRq></ItemInventoryQueryRq>\n  </QBPOSXMLMsgsRq>\n</QBPOSXML>"
+    response = qb.ProcessRequest(ticket, xmlstream)
+    return respnonce
 
 #this is where the magic happens.
-#First, we need to determine if this is running with 64-bit python, because quickbooks is 32-bit.
-if (( 8 * struct.calcsize("P")) == '64'):
+if (( 8 * struct.calcsize("P")) == '64'): #check for a 32 bit address space, because the quickbooks I'm developing on is 32-bit only. The more I work on this, the more I am reminded that this is OLD software.
     print("ERROR: Quickbooks POS is a 32-bit ONLY application. \n This will prevent this library from accessing Quickbooks POS \n Please run this in a 32 bit address space.")
