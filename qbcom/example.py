84421279b212d8d2d62e0a1b10b4b03486fe90f1
pcs3rd <rhean1620@gmail.com>
2020-07-20 12:21:36 -0400

    get actual conversion to json

--- qbcom/example.py
+++ qbcom/example.py
@@ -1,10 +1,13 @@
 import qbcom as pyqb
 import xmltodict
+import json
 print("open connection")
 qb = pyqb.open()
 tk = pyqb.begin(qb)
 print("Fetch info for item 43313 in json format\n\n")
-print(xmltodict.parse(pyqb.itemquery(qb, tk, 43313), dict_constructor=dict, encoding='utf-8'))
+dictobject = xmltodict.parse(pyqb.itemquery(qb, tk, 43313), dict_constructor=dict, encoding='utf-8')
+jsonobject = json.dumps(dictobject)
+print(jsonobject)
 print("Fetch info for item 43313 in xml format\n\n")
 print(pyqb.itemquery(qb, tk, 43313))
 pyqb.close(qb, tk)
